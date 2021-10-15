ebf01a68d3b4c5cae0c3710ba95b79b0c39daa99
eva <83175995+evapopp@users.noreply.github.com>
2021-10-15 00:13:25 -0700

    deleting console logs and cleaning up script

--- public/index.js
+++ public/index.js
@@ -21,34 +21,24 @@ document.addEventListener('DOMContentLoaded', () => {
 function listBreweries(response) {
     function buildBrewList(brew){
         const brewNameList = document.createElement('div');
-        // const brewStreetEl = document.createElement('div');
-        // const brewNumberEl = document.createElement('div');
-        // const brewWebsiteEl = document.createElement('div');
         brewNameList.classList.add('brew.id');
-        // brewStreetEl.classList.add(brew.id);
-        // brewNumberEl.classList.add(brew.id);
-        // brewWebsiteEl.classList.add(brew.id);
-        // console.log(brewStreetEl)
-
-        console.log(brewNumberEl)
-        console.log(brew);
-        console.log(brew.name)
+        
         const brewName = brew.name;
         const brewStreet = brew.street;
+        const brewCity = brew.city;
+        const brewState = brew.state;
         const brewNumber = brew.phone;
         const brewWebsite = brew.website_url;
 
         brewNameList.innerHTML = brewName
-        console.log(brewNameList)
-
         cardContainerEl.append(brewNameList);
 
         brewNameList.addEventListener('click', async (event) => {
             const test = await event;
             cardContainerEl.remove();
-            console.log(brew)
+
             brewNameEl.innerHTML = brewName;
-            brewStreetEl.innerHTML = brewStreet;
+            brewStreetEl.innerHTML = brewStreet + ' ' + brewCity + ', ' + brewState;
             brewNumberEl.innerHTML = brewNumber;
             brewWebsiteEl.innerHTML = brewWebsite;
             brewWebsiteEl.setAttribute('href', `${brewWebsite}`);
